6ea96d450f6d388a95cd4a842f24109ebcaeee8b
Kasembeli8865 <lionel.kasembeli@student.moringaschool.com>
2023-07-04 16:02:17 +0300

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-fetch
+function bookNow(){
+    fetch('http://localhost:3000/Routes')
+    .then( response => response.json())
+    .then(data => displayTickets(data))
+}
+
+function displayTickets(ticket){
+    const listings = document.querySelector("#listings")
+     ticket.forEach(element => {
+        let div = createElement ("div")
+        let availableTicket = document.createElement("h2")
+        availableTicket.innerText = element.destination
+        div.appendChild(availableTicket);
+
+        let departureTime = document.createElement("h3")
+        departureTime.innerHTML = "Departure Time :" + element.time
+        div.appendChild(departureTime);
+
+        let busCompany = document.createElement("h3")
+        busCompany.innerText = element.company
+        div.appendChild(busCompany);
+
+        let availableSeats = document.createElement("h3")
+        availableSeats.innerText = "Available Seats :" + element.seats
+        div.appendChild(availableSeats);
+
+        let ticketPrice = document.createElement("button")
+        ticketPrice.innerText = element.price
+        div.appendChild(ticketPrice)
+
+        ticketPrice.addEventListener("click", alert('TICKET PURCHASED'))
+    });
+}
