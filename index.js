e989656af55a841bdbfb7e230f74df7861d5f6f1
Kasembeli8865 <lionel.kasembeli@student.moringaschool.com>
2023-07-04 22:30:29 +0300

    Almost done

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
-function bookNow(){
+function bookNow() {
     fetch('http://localhost:3000/Routes')
-    .then( response => response.json())
-    .then(data => displayTickets(data))
-}
+      .then(response => response.json())
+      .then(data => displayTickets(data));
+  }
+  // Function to display tickets
+  function displayTickets(ticket) {
+      const listings = document.getElementById("listings"); // Get the listings container element from the DOM
+  
+      // Iterate over each ticket
+      ticket.forEach(element => {
+          let div = document.createElement("div"); // Create a new div element for each ticket
+          div.classList.add("ticket-item")
+          let availableTicket = document.createElement("h2"); // Create a new h2 element for the ticket destination
+          availableTicket.innerText = element.destination; // Set the text of the h2 element to the ticket destination
+          div.appendChild(availableTicket); // Append the h2 element to the ticket div
+  
+          let departureTime = document.createElement("h3"); // Create a new h3 element for the departure time
+          departureTime.innerHTML = "Departure Time: " + element.time; // Set the innerHTML of the h3 element to the departure time
+          div.appendChild(departureTime); // Append the h3 element to the ticket div
 
-function displayTickets(ticket){
-    const listings = document.querySelector("#listings")
-     ticket.forEach(element => {
-        let div = createElement ("div")
-        let availableTicket = document.createElement("h2")
-        availableTicket.innerText = element.destination
-        div.appendChild(availableTicket);
-
-        let departureTime = document.createElement("h3")
-        departureTime.innerHTML = "Departure Time :" + element.time
-        div.appendChild(departureTime);
-
-        let busCompany = document.createElement("h3")
-        busCompany.innerText = element.company
-        div.appendChild(busCompany);
-
-        let availableSeats = document.createElement("h3")
-        availableSeats.innerText = "Available Seats :" + element.seats
-        div.appendChild(availableSeats);
-
-        let ticketPrice = document.createElement("button")
-        ticketPrice.innerText = element.price
-        div.appendChild(ticketPrice)
-
-        ticketPrice.addEventListener("click", alert('TICKET PURCHASED'))
-    });
-}
+  
+          let busCompany = document.createElement("h4"); // Create a new h3 element for the bus company
+          busCompany.innerText = element.company; // Set the text of the h3 element to the bus company
+          div.appendChild(busCompany); // Append the h3 element to the ticket div
+  
+          let availableSeats = document.createElement("h5"); // Create a new h3 element for the available seats
+          availableSeats.tickets = element.seats - element.ticketsSold; // Calculate the number of available seats
+          availableSeats.innerText = "Available Seats: " + availableSeats.tickets; // Set the text of the h3 element to the number of available seats
+          div.appendChild(availableSeats); // Append the h3 element to the ticket div
+  
+          let ticketPrice = document.createElement("button"); // Create a new button element for the ticket price
+          ticketPrice.innerText = element.price; // Set the text of the button element to the ticket price
+          div.appendChild(ticketPrice); // Append the button element to the ticket div
+  
+          // Add event listener to purchase ticket on button click
+          ticketPrice.addEventListener("click", function () {
+              purchaseTicket(element, availableSeats); // Call the purchaseTicket function with the ticket and available seats as arguments
+          });
+  
+          // Append the ticket div to the listings container
+          listings.appendChild(div);
+      });
+  }
+  
+ // This function is responsible for purchasing a ticket
+ function purchaseTicket(ticket, availableSeats) {
+   // Check if there are available tickets
+   if (availableSeats.tickets > 0) {
+     // Decrease the number of available tickets by 1
+     availableSeats.tickets--;
+     
+     // Update the text content of the availableSeats element with the new count
+     availableSeats.innerText = "Available Seats: " + availableSeats.tickets;
+     
+     // Display a pop-up message showing the details of the purchased ticket
+     alert("TICKET PURCHASED\nDestination: " + ticket.destination + "\nPrice: " + ticket.price);
+   } else {
+     // Display a pop-up message indicating that no tickets are available
+     alert("No Tickets Available!");
+   }
+ }
+ 
+  
